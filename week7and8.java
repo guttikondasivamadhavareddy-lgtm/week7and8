5302bdc78e0f6298860cddcb0b5b604e959ebb33
guttikondasivamadhavareddy-lgtm <guttikondasivamadhavareddy@gmail.com>
2026-04-30 20:57:20 +0530

    level 2 question 1 completed

--- week7and8.java
+++ week7and8.java
@@ -2,27 +2,60 @@ import java.util.Scanner;
 
 public class week7and8 {
 
-    static int checkNumber(int num) {
-        if (num > 0) return 1;
-        else if (num < 0) return -1;
-        else return 0;
+    static int[] getFactors(int num) {
+        int count = 0;
+
+        // Count factors
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+
+        // Store factors
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        return factors;
+    }
+
+    static int sum(int[] arr) {
+        int s = 0;
+        for (int x : arr) s += x;
+        return s;
+    }
+
+    static int product(int[] arr) {
+        int p = 1;
+        for (int x : arr) p *= x;
+        return p;
+    }
+
+    static double sumOfSquares(int[] arr) {
+        double s = 0;
+        for (int x : arr) {
+            s += Math.pow(x, 2);
+        }
+        return s;
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter number: ");
         int num = sc.nextInt();
 
-        int result = checkNumber(num);
+        int[] factors = getFactors(num);
 
-        if (result == 1) {
-            System.out.println("Positive number");
-        } else if (result == -1) {
-            System.out.println("Negative number");
-        } else {
-            System.out.println("Zero");
-        }
+        System.out.print("Factors: ");
+        for (int f : factors) System.out.print(f + " ");
+
+        System.out.println("\nSum: " + sum(factors));
+        System.out.println("Product: " + product(factors));
+        System.out.println("Sum of squares: " + sumOfSquares(factors));
     }
 }
